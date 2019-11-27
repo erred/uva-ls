0bd928b37133a94aa90e76b5432711bfc81cf17b
Sean Liao <seankhliao@gmail.com>
2019-11-27 17:29:04 +0100

    decode

--- app/awslambda/awslambda.py
+++ app/awslambda/awslambda.py
@@ -15,10 +15,10 @@ def handler(event, context):
     t = time.time_ns()
     tt = time.thread_time_ns()
 
-    post_data = event["body"]
-
-    if event["isBase64Encoded"]:
-        post_data = base64.b64decode(post_data)
+    post_data = base64.b64decode(event["body"])
+    # post_data = event["body"]
+    # if event["isBase64Encoded"]:
+    #     post_data = base64.b64decode(post_data)
 
     im = Image.open(io.BytesIO(post_data))
     im = im.resize((SIZE, SIZE))
