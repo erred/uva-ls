4078ad5e201f3a72d27face2793e728131a21711
Sean Liao <seankhliao@gmail.com>
2019-11-27 14:56:17 +0100

    update

--- app/awslambda.py
+++ app/awslambda.py
@@ -11,38 +11,30 @@ SIZE = int(os.getenv("SIZE", "256"))
 SERVERID = uuid.uuid4()
 
 def handler(event, context):
-    try:
-        t = time.time_ns()
-        tt = time.thread_time_ns()
+    t = time.time_ns()
+    tt = time.thread_time_ns()
 
-        post_data = base64.decodebytes(event["body"])
+    post_data = base64.decodebytes(event["body"])
 
-        im = Image.open(io.BytesIO(post_data))
-        im = im.resize((SIZE, SIZE))
-        buf = io.BytesIO()
-        im.save(buf, format='jpg')
-        buf.seek(0)
+    im = Image.open(io.BytesIO(post_data))
+    im = im.resize((SIZE, SIZE))
+    buf = io.BytesIO()
+    im.save(buf, format='jpg')
+    buf.seek(0)
 
-        t = time.time_ns() - t
-        tt = time.thread_time_ns() - tt
+    t = time.time_ns() - t
+    tt = time.thread_time_ns() - tt
 
-        res = {
-            "statusCode": 200,
-            "statusDescription": "200 OK",
-            "headers": {
-                "Time": str(t),
-                "Thread-Time": str(tt),
-                "Server-UUID": SERVERID,
-                "Content-Type": "image/jpg",
-            },
-            "isBase64Encoded": True,
-            "body": base64.encodebytes(buf.read())
-        }
-        print(res.headers)
-    # except Exception as e:
-    #     traceback.print_tb(e)
-    #     res = {
-    #         "statusCode": 500,
-    #         "statusDescription": str(e),
-    #     }
+    res = {
+        "statusCode": 200,
+        "statusDescription": "200 OK",
+        "headers": {
+            "Time": str(t),
+            "Thread-Time": str(tt),
+            "Server-UUID": SERVERID,
+            "Content-Type": "image/jpg",
+        },
+        "isBase64Encoded": True,
+        "body": base64.encodebytes(buf.read())
+    }
     return res
