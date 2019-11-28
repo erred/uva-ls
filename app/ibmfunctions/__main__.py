a5965f7143a7e6f1241681b040917e77fd929e19
Sean Liao <seankhliao@gmail.com>
2019-11-28 15:58:58 +0100

    update

--- app/ibmfunctions/__main__.py
+++ app/ibmfunctions/__main__.py
@@ -10,38 +10,30 @@ SIZE = int(os.getenv("SIZE", "256"))
 SERVERID = str(uuid.uuid4())
 
 def main(args):
-    try:
-        t = time.time_ns()
-        tt = time.thread_time_ns()
+    t = time.time_ns()
+    tt = time.thread_time_ns()
 
-        post_data = event["body"]
-        if event["isBase64Encoded"]:
-            post_data = base64.decodebytes(post_data)
+    post_data = args["body"]
+    if event["isBase64Encoded"]:
+        post_data = base64.decodebytes(post_data)
 
-        im = Image.open(io.BytesIO(post_data))
-        im = im.resize((SIZE, SIZE))
-        buf = io.BytesIO()
-        im.save(buf, format='JPEG')
-        buf.seek(0)
+    im = Image.open(io.BytesIO(post_data))
+    im = im.resize((SIZE, SIZE))
+    buf = io.BytesIO()
+    im.save(buf, format='JPEG')
+    buf.seek(0)
 
-        t = time.time_ns() - t
-        tt = time.thread_time_ns() - tt
+    t = time.time_ns() - t
+    tt = time.thread_time_ns() - tt
 
-        res = {
-            "statusCode": 200,
-            "headers": {
-                "Time": str(t),
-                "Thread-Time": str(tt),
-                "Server-UUID": SERVERID,
-                "Content-Type": "image/jpeg",
-            },
-            "body": base64.encodebytes(buf.read())
-        }
-        print(res.headers)
-    except Exception as e:
-        print(str(e))
-        res = {
-            "statusCode": 500,
-            "body":str(e)
-        }
+    res = {
+        "statusCode": 200,
+        "headers": {
+            "Time": str(t),
+            "Thread-Time": str(tt),
+            "Server-UUID": SERVERID,
+            "Content-Type": "image/jpeg",
+        },
+        "body": base64.encodebytes(buf.read())
+    }
     return res
