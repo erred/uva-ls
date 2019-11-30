537aeb0b42e71233c15a24a1c61390a7d35c033d
Sean Liao <seankhliao@gmail.com>
2019-11-30 17:48:24 +0100

    downloader

---
+++ tools/photodownloader/go.mod
@@ -0,0 +1,8 @@
+module photo.downloader
+
+go 1.13
+
+require (
+	github.com/hbagdi/go-unsplash v0.0.0-20191020055717-140ca16b8530
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+)
---
+++ tools/photodownloader/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/hbagdi/go-unsplash/unsplash"
+)
+
+type myTransport struct{}
+
+func (t *myTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add("authorization", "Client-ID "+os.Getenv("UNSPLASH"))
+	return http.DefaultTransport.RoundTrip(req)
+}
+
+func main() {
+	client := &http.Client{
+		Transport: &myTransport{},
+	}
+
+	u := unsplash.New(client)
+	o := &unsplash.ListOpt{Page: 1, PerPage: 30}
+	if !o.Valid() {
+		log.Println("options not valid")
+	}
+
+	var data [][]string
+	defer func() {
+		if len(data) > 0 {
+			f, err := os.Create("photos.csv")
+			if err != nil {
+				log.Printf("create file: %v", err)
+				return
+			}
+			defer f.Close()
+
+			w := csv.NewWriter(f)
+			w.WriteAll(data)
+			w.Flush()
+			fmt.Printf("written %d records\n", len(data))
+
+		}
+	}()
+
+	for len(data) < 600 {
+		photos, res, err := u.Photos.All(o)
+		if err != nil {
+                        if _, ok := err.(*unsplash.RateLimitError); ok {
+                                log.Println("rate limited, sleeping for 10min")
+                                time.Sleep(10 * time.Minute)
+                                continue
+                        }
+                        log.Println("other err: %v", err)
+                        return
+		}
+
+		for _, p := range *photos {
+			if *p.Width > 4000 && *p.Height > 4000 {
+				data = append(data, []string{strconv.Itoa(*p.Width), strconv.Itoa(*p.Height), *p.ID})
+			}
+		}
+		fmt.Println("current count: ", len(data))
+
+		if !res.HasNextPage {
+			break
+		}
+		o.Page = res.NextPage
+	}
+}
